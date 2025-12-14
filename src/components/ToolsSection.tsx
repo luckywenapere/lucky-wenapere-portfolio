96ba5db2be93718d29089473456fa1d284a6b536
unknown <luckydio10@gmail.com>
2025-12-14 20:46:06 +0100

    removed icons

--- src/components/ToolsSection.tsx
+++ src/components/ToolsSection.tsx
@@ -291,18 +291,7 @@ export default function ToolsSection() {
               <span className="text-yellow-400 text-sm font-medium text-center">Slack</span>
             </div>
 
-            <div className="flex flex-col items-center group">
-              <div className="w-16 h-16 mb-3 flex items-center justify-center">
-                <Image
-                  src="/img/tool-logos/zoom.png"
-                  alt="Zoom"
-                  width={64}
-                  height={64}
-                  className="group-hover:scale-110 transition-transform duration-300"
-                />
-              </div>
-              <span className="text-yellow-400 text-sm font-medium text-center">Zoom</span>
-            </div>
+            
 
             <div className="flex flex-col items-center group">
               <div className="w-16 h-16 mb-3 flex items-center justify-center">
